50ae68f1e0916739b504ee2814bfa557e0e68996
dpankov91 <dpankov91@gmail.com>
2023-03-11 09:51:30 +0200

    authentication

---
+++ atrium-backend/AuthenticationWebApi/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using JwtAuthenticationManager;
+using JwtAuthenticationManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtTokenHandler _jwtTokenHandler;
+
+        public AccountController(JwtTokenHandler jwtTokenHandler)
+        {
+            _jwtTokenHandler = jwtTokenHandler;
+        }
+
+        [HttpPost]
+        public ActionResult<AuthenticationResponse?> Authenticate([FromBody] AuthenticationRequest autjenticationRequest)
+        {
+            var authenticationResponse = _jwtTokenHandler.GenerateJwtToken(autjenticationRequest);
+            if (authenticationResponse == null) return Unauthorized();
+            return authenticationResponse;
+        }
+    }
+}
